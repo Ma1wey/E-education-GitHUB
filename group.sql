5ee9733478613f016997059e0a5991f1549a98b9
Ma1wey <matwej_ors@mail.ua>
2016-06-02 22:31:54 +0300

    Update group.sql

--- group.sql
+++ group.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `e-education`.`group` (
-  `groupID` INT NOT NULL,
+  `university groupID` INT NOT NULL,
   `groupName` VARCHAR(45) NULL,
   `groupAbbr` VARCHAR(45) NULL,
   `groupStartYear` VARCHAR(45) NULL,
-  PRIMARY KEY (`groupID`));
+  PRIMARY KEY (`university groupID`));
